824c93bc4022ebe08694832ba26df1e5f1443977
Daniel Jordan <daniel.jordan1@ucdconnect.ie>
2017-02-26 22:55:43 +0000

    added generate() and get_input()

--- led_tester/main.py
+++ led_tester/main.py
@@ -1,15 +1,36 @@
-# import argparse
-#
-# # parses arguments (?)
-# parser = argparse.ArgumentParser()
-# parser.add_argument('--input', help='input help')
-# args = parser.parse_args()
-#
-# filename = args.input
+import argparse
+import urllib.request
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', help='input help')
+    args = parser.parse_args()
+    filename = args.input
+
+    return filename
+
+
+def get_input(fname):
+    uri = fname
+    req = urllib.request.urlopen(uri)
+    buffer = req.read().decode('utf-8').split("\n")
+
+    array_size = buffer[0]
+    instructions = buffer[1:]
+
+    return array_size, instructions
+
+
+def generate(array_size):
+    myarray = [[True for x in range(array_size)] for y in range(array_size)]
+    return myarray
+
 
 
 def add(x, y):
     return x + y
 
+
 if __name__=="__main__":
-    print(add(5, 5))
+    # get_input("http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt")
+    pass
--- tests/test.py
+++ tests/test.py
@@ -4,4 +4,17 @@ from led_tester import main
 def first_test():
     assert main.add(2, 2) == 4
     assert main.add(5, 5) == 10
-    assert main.add(1, 1) == 3
+    assert main.add(1, 1) == 2
+
+
+def generate_test():
+    size = 10
+    myarray = main.generate(size)
+    assert len(myarray) == size
+
+    for i in range(size):
+        assert len(myarray[i]) == size
+        for j in range(size):
+            assert myarray[i][j] == True
+
+generate_test()
