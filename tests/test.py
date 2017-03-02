20ef024d9192fcaefd373f19761205977afc6784
Daniel Jordan <daniel.jordan1@ucdconnect.ie>
2017-03-02 17:40:45 +0000

    Fixed tests

--- tests/test.py
+++ tests/test.py
@@ -1,74 +1,68 @@
 from led_tester import main
+import numpy as np
 
 
 def test_generate():
     size = 10
-    myarray = main.Led(size)
-
-    for i in range(size):
-        for j in range(size):
-            assert myarray.led_array[i][j] == False
+    led = main.Led(size)
+    assert np.all(led.led_array[0:size + 1, 0:size + 1] == False)
 
 
 def test_off():
     size = 10
     led = main.Led(size)
-    led.turn_off(0, 0, size, size)
-    for i in range(size):
-        for j in range(size):
-            assert led.led_array[i][j] == False
+    assert np.all(led.led_array[0:size + 1, 0:size + 1] == False)
 
     led.turn_on(0, 0, size, size)
-    led.turn_off(size, size, 0, 0)
-    for i in range(size):
-        for j in range(size):
-            assert led.led_array[i][j] == False
+    led.turn_off(0, 0, size, size)
+    assert np.all(led.led_array[0:size + 1, 0:size + 1] == False)
 
-
+    led.turn_on(0, 0, size, size)
+    led.turn_off(0, 0, 4, 4)
+    assert led.number_on() == 75
 
 
 def test_on():
     size = 10
     led = main.Led(size)
-    led.turn_off(0, 0, size, size)
     led.turn_on(0, 0, size, size)
-    for i in range(size):
-        for j in range(size):
-            assert led.led_array[i][j] == True
+    assert np.all(led.led_array[0:size + 1, 0:size + 1])
 
     led.turn_off(0, 0, size, size)
-    led.turn_on(size, size, 0, 0)
-    for i in range(size):
-        for j in range(size):
-            assert led.led_array[i][j] == True
+    led.turn_on(0, 0, 4, 4)
+    assert led.number_on() == 25
 
 
 def test_switch():
     size = 10
     led = main.Led(size)
     led.switch(0, 0, size, size)
-    for i in range(size):
-        for j in range(size):
-            assert led.led_array[i][j] == False
+    assert np.all(led.led_array[0:size + 1, 0:size + 1])
 
     led.switch(0, 0, size, size)
-    for i in range(size):
-        for j in range(size):
-            assert led.led_array[i][j] == True
+    assert np.all(led.led_array[0:size + 1, 0:size + 1] == False)
+
+    led.switch(0, 4, 0, 4)
+
+    assert np.all(led.led_array[0:0, 4:4])
 
 
 def test_number_on():
     size = 10
     led = main.Led(size)
+    led.turn_on(0, 0, size, size)
     assert led.number_on() == size**2
 
     led.turn_off(0, 0, size, size)
     assert led.number_on() == 0
 
+    led.turn_on(0, 0, 4, 4)
+    assert led.number_on() == 25
+
 
 def test_sanitise():
     size = 10
     led = main.Led(size)
-    assert led.sanitize([-5, 0, 5, 10, 15]) == [0, 0, 5, 10, 10]
+    assert led.sanitize([-10, -5, 0, 5, 10, 15, 20]) == [0, 0, 0, 5, 10, 10, 10]
 
 
